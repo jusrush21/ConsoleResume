2b5536cd47788f9c068155f11654f72143a05555
jusrush21 <reachjustin212@gmail.com>
2021-05-17 16:48:27 -0500

    first commit

---
+++ ConsoleResume.js
@@ -0,0 +1,25 @@
+console.log("Name: Justin Rush")
+console.log("Career: IT / Computer Science")
+console.log("Description: I'm 24 years old, and I enjoy making stuff.")
+console.log("Intrest: I like to watch movies, play video games, and listen to music.")
+console.log("Previous Employment: GameStop; Cashier: I helped people find what they need and then check them out at the cashier. AT&T; Sales associate, I helped people find what they were looking for...similar to GameStop. Amazon; Delivery Driver, I delievered packages to people.")
+console.log("Skills: Creative, Patient, Team Worker, BAM: WEBSITE DEVELOPER")
+
+let myName = ("Justin Rush")
+let GameStop = ("GameStop; Cashier: I helped people find what they need and then check them out at the cashier.")
+let att = ("AT&T; Sales associate, I helped people find what they were looking for...similar to GameStop.")
+let Amazon = ("Amazon; Delivery Driver, I delievered packages to people.")
+let jobs = [GameStop, att, Amazon]
+
+
+//let prevEmploy = ("GameStop; Cashier: I helped people find what they need and then check them out at the cashier. AT&T; Sales associate, I helped people find what they were looking for...similar to GameStop. Amazon; Delivery Driver, I delievered packages to people.")
+
+
+function displayPosition(name, jobTitle,) {
+    console.log(GameStop)
+    console.log(att);
+    console.log(Amazon);
+}
+
+
+displayPosition(myName,);
